7df613f67dfceaa7105c5a2b7b46b952d708f6f6
Gemri <gemrisalulog4@gmail.com>
2026-08-16 00:12:44 +0400

    final

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -37,7 +37,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 8 * 60 * 60 * 1000 // 8 hours
     })
 
